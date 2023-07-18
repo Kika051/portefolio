5d486828303888019c06e7ad1cb6beff7f59ff74
Florian GERON <geron-f@outlook.fr>
2023-07-18 09:33:21 +0200

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
+    <div>
+      <h1>HELLO WORLD</h1>
     </div>
   );
 }
